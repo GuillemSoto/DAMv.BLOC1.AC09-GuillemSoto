ae41041fb4eb8595bd1b79c5aaa9c843e0bf7b4b
GuillemSoto <guillem.soto.7e8@itb.cat>
2025-11-11 11:53:41 +0100

    Exercise four completed

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿public class Program
+public class Program
 {
     public static void Main()
     {
@@ -20,11 +20,71 @@
                 validNumber = false;
                 Console.WriteLine(InvalidNumber);
             }
-            
+
         } while (!validNumber);
         switch (menuNum)
         {
-            case 1:
+            case 4:
+                const string SearchMsg = "Which number do you want to search?";
+                const string DoesExist = "The number exists!";
+                const string DoesntExist = "The number you entered does not exist";
+                int userSearch = 0;
+                int[] arr = new int[10];
+                int temp = 0;
+                bool exists = false;
+                Random ran = new Random();
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = ran.Next(1, 10);
+                }
+                for (int write = 0; write < arr.Length; write++)
+                {
+                    for (int sort = 0; sort < arr.Length - 1; sort++)
+                    {
+                        if (arr[sort] > arr[sort + 1])
+                        {
+                            temp = arr[sort + 1];
+                            arr[sort + 1] = arr[sort];
+                            arr[sort] = temp;
+                        }
+                    }
+                }
+
+                
+
+                Console.WriteLine();
+                Console.WriteLine(SearchMsg);
+                do
+                {
+                    try
+                    {
+                        userSearch = int.Parse(Console.ReadLine());
+                        validNumber = true;
+                    }
+                    catch
+                    {
+                        validNumber = false;
+                        Console.WriteLine(InvalidNumber);
+                    }
+
+                } while (!validNumber);
+                foreach (int num in arr)
+                {
+                    if(num == userSearch)
+                    {
+                        exists = true;
+                    }
+                }
+                if (exists)
+                {
+                    Console.WriteLine(DoesExist);
+                }
+                else
+                {
+                    Console.WriteLine(DoesntExist);
+                }
+                for (int i = 0; i < arr.Length; i++)
+                    Console.Write(arr[i] + " ");
                 break;
             default:
                 Console.WriteLine(ExitMsg);
